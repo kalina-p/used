bf789e6bcc31fda7d09e2ab5127264450af8b577
Paweł <78249685+kalina-p@users.noreply.github.com>
2022-02-16 16:59:24 +0100

    Update sgg.py

--- sgg.py
+++ sgg.py
@@ -11,20 +11,18 @@ time.sleep(5)
 
 #dodaj = window.find_element_by_xpath("/html/body/div/section/div/div/div/div[1]/div[3]/div[3]/form/div[4]/div/input[3]")
 dodaj_do_koszyka = window.find_element_by_xpath("/html/body/div/section/div/div/div/div[1]/div[3]/div[3]/form/button")
-(czy_aktywny_koszyk) = dodaj_do_koszyka.get_property('disabled') # zwykle wartość True
+czy_aktywny_koszyk = dodaj_do_koszyka.get_property('disabled') # zwykle wartość True
 #aktywny_koszyk = window.find_element_by_xpath("")
 #final_koszyk = window.find_element_by_xpath("/html/body/div/header/div/div/div[4]/div/nav/ul/li[5]/a")
 
 
 
 while True:
-    # sprawdza czy guzik na stronie "dodaj do koszyka" jest aktywny. Jeżeli nie to niech zrobi refresh strony i ponownie zrobi petle
-    czy_aktywny_koszyk == True
-    print("pierwsza")
-    time.sleep(5)
-    window.refresh()
-    if czy_aktywny_koszyk != True: 
-        # jeżeli guzik jest aktywny wtedy wykonaj poniższe czynności
+    if czy_aktywny_koszyk == True:
+        print("pierwsza")
+        time.sleep(5)
+        window.refresh()
+    elif czy_aktywny_koszyk != True:
         print("druga")
         # for i in range(ilosc - 1): # określ ilośc ile ma wybrać ?
         #     dodaj.click()
@@ -33,3 +31,4 @@ while True:
       # akcyza= Select(window.find_element_by_xpath('/html/body/div/section[2]/div/div[1]/div[3]/div[1]/div[2]/div/select'))
       # akcyza.select_by_index(1) # strona w koszyku akcyza
       # window.find_element_by_xpath("/html/body/div[1]/section[2]/div/div[1]/div[3]/div[2]/div[2]/button").click() # przejdź do kasy
+      # break
